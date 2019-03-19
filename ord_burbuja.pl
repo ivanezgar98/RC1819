155017652e76562ae625be3c85f9d985bbe431a2
ivanezgar98 <ivan.perez798@alu.uhu.es>
2019-03-19 14:30:02 +0100

    ord_burbuja

--- ord_burbuja.pl
+++ ord_burbuja.pl
@@ -2,10 +2,11 @@
 %es cierto si R unifica con una lista que contiene
 %los elementos de Lista ordenados de menor a mayor
 %Lista puede contener elementos repetidos.
-
+ord_burbuja(Lista,R):- ordenada(R).
+ord_burbuja(E1, E2|Resto).
 % ordenada(+Lista)
 % es cierto si los elementos de Lista estan ordenados de
 % menor a mayor
 ordenada([]).
 ordenada([_]).
-ordenada(Lista):- append(_, [E1, E2|_], Lista) , E1 =< E2.
+ordenada(Lista): E1 =< E2, ordenada([E2|Resto]).
